bb92adbd5fbd73b9f477ecb4470b1fac27091550
dhruvkhai <dhruvkhaitan@gmail.com>
2024-11-10 10:49:27 +0530

    its the first commit of this repo.

---
+++ Know-Police/app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+const index = ( ) => {
+  return (
+    <view style = {{backgroundColor: "green",
+      flex : 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <text>hi i am dhruv</text>
+    </view>
+  )
+}
